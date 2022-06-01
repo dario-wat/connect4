d914aaea778a6c33f9f7ad3e4fb09a07b94c243d
Dario <dario.vidas92@gmail.com>
2022-05-31 18:21:14 -0700

    Draw the board

--- js/connect4.js
+++ js/connect4.js
@@ -1,12 +1,47 @@
-function draw() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+// Dimensions of the board
+const DISC_R = 50;
+const HOLE_SPACE_X = 30;
+const HOLE_SPACE_Y = 15;
+const BOARD_WIDTH = 7;
+const BOARD_HEIGHT = 6;
+const BOARD_START_X = 50;
+const BOARD_START_Y = 50;
 
-    ctx.fillRect(25, 25, 600, 100);
-    ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+function xCoord(column) {
+  return BOARD_START_X + (column + 1) * HOLE_SPACE_X + column * 2 * DISC_R + DISC_R;
+}
+
+function yCoord(row) {
+  return BOARD_START_Y + (row + 1) * HOLE_SPACE_Y + row * 2 * DISC_R + DISC_R;
+}
+
+function drawBoard() {
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(
+    BOARD_START_X, 
+    BOARD_START_Y,
+    BOARD_WIDTH * 2 * DISC_R + (BOARD_WIDTH + 1) * HOLE_SPACE_X, 
+    BOARD_HEIGHT * 2 * DISC_R + (BOARD_HEIGHT + 1) * HOLE_SPACE_Y,
+  );
+
+  for (r = 0; r < BOARD_HEIGHT; r++) {
+    for (c = 0; c < BOARD_WIDTH; c++) {
+      ctx.beginPath();
+      ctx.arc(
+        xCoord(c),
+        yCoord(r),
+        DISC_R,       // radius
+        0,            // start angle
+        2 * Math.PI,  // end angle
+      );
+      ctx.fillStyle = 'white';
+      ctx.fill();
+    }
   }
 }
 
-draw();
+drawBoard();
