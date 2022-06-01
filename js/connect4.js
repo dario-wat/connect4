e4c4e15d142b3b47b5718f86f911785f18c85f83
Dario <dario.vidas92@gmail.com>
2022-05-31 20:08:17 -0700

    Add bounding rectangle drawing for the hovered column

--- js/connect4.js
+++ js/connect4.js
@@ -11,20 +11,33 @@ const BOARD_START_Y = 50;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+// X coordinate of the center of the disc
 function xCoord(column) {
   return BOARD_START_X + (column + 1) * HOLE_SPACE_X + column * 2 * DISC_R + DISC_R;
 }
 
+// Y coordinate of the center of the disc
 function yCoord(row) {
   return BOARD_START_Y + (row + 1) * HOLE_SPACE_Y + row * 2 * DISC_R + DISC_R;
+}
+
+// Column given X coordinate
+function colFromX(x) {
+  var fCol = (x - BOARD_START_X - HOLE_SPACE_X / 2) / (2 * DISC_R + HOLE_SPACE_X);
+  return fCol < 0 || fCol >= BOARD_WIDTH ? -1 : Math.trunc(fCol);
+}
+
+function drawBackground() {
+  ctx.fillStyle = 'grey';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function drawBoard() {
   ctx.fillStyle = 'blue';
   ctx.fillRect(
-    BOARD_START_X, 
+    BOARD_START_X + HOLE_SPACE_X / 2, 
     BOARD_START_Y,
-    BOARD_WIDTH * 2 * DISC_R + (BOARD_WIDTH + 1) * HOLE_SPACE_X, 
+    BOARD_WIDTH * 2 * DISC_R + BOARD_WIDTH * HOLE_SPACE_X, 
     BOARD_HEIGHT * 2 * DISC_R + (BOARD_HEIGHT + 1) * HOLE_SPACE_Y,
   );
 
@@ -44,4 +57,34 @@ function drawBoard() {
   }
 }
 
-drawBoard();
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBackground();
+  drawBoard();
+}
+
+function onMouseUpdate(e) {
+  redraw();
+  var x = e.pageX;
+  var y = e.pageY;
+
+  var col = colFromX(x);
+  // console.log(col);
+  if (col == -1) {
+    return;
+  }
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = 'red';
+  ctx.rect(
+    xCoord(col) - DISC_R - HOLE_SPACE_X / 2, 
+    BOARD_START_Y,
+    2 * DISC_R + HOLE_SPACE_X, 
+    BOARD_HEIGHT * 2 * DISC_R + (BOARD_HEIGHT + 1) * HOLE_SPACE_Y,
+  );
+  ctx.stroke();
+}
+
+document.addEventListener('mousemove', onMouseUpdate, false);
+
+redraw();
